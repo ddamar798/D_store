7b500325ca6937a3254261125fc01de44f56f28e
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-02-18 20:18:58 +0700

    feat: fill Models Brands

--- app/Models/Brands.php
+++ app/Models/Brands.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use illuminate\Support\str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Brands extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'logo',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attribute['name'] = $value;
+        $this->attribute['slug'] = Str::slug($value);
+    }
+
+    public function shoes(): HasMany{
+        return $this->HasMany(Shoes::class);
+    }
 }

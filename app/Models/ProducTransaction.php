efc0056c3d808d82dffb5a6a48cf59da5ce53ac2
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-02-19 19:16:44 +0700

    feat: fill Models ProductTransaction

--- app/Models/ProducTransaction.php
+++ app/Models/ProducTransaction.php
@@ -3,8 +3,43 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProducTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'booking_trx_id',
+        'city',
+        'post_code',
+        'address',
+        'sub_tottal_amount',
+        'grand_total_amount',
+        'discount_amount',
+        'is_paid',
+        'shoe_id',
+        'shoe_size',
+        'promo_code_id',
+        'proof',
+    ];
+
+    // Untuk membuat random code Booking transaksi.
+    public static function generateUniqueTrxId(){
+        $prefix = 'DS';
+        do {
+            $randomString = $prefix . mt_rand(1000, 9999); // artinya (var 'DS' + Angka randm).
+        } while (self::where('booking_trx_id', $randomString)->exits()); // cek apakah transaction ada/tidak.
+
+        return $randomString; // jika ada akan masuk kesini.
+    }
+
+    public function promoCode(): BelongsTo{
+        return $this->belongsTo(PromoCode::class, 'promo_code_id');
+    }
 }

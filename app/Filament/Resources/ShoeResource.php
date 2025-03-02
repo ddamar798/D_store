faff66af7cfcccdf21061b51e37ef66fd8a41b2a
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-03-02 19:02:41 +0700

    feat: add comment

--- app/Filament/Resources/ShoeResource.php
+++ app/Filament/Resources/ShoeResource.php
@@ -113,7 +113,7 @@ class ShoeResource extends Resource
             ])
             ->filters([
                 //
-                SelectFilter::make('category_id')
+                SelectFilter::make('category_id') // untuk memfilter data berdasarkan Category. Bisa juga diganti denggan Resource dari Brand
                 ->label('Category')
                 ->relationship('category', 'name'),
             ])
